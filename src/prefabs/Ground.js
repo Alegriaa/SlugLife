9617bcb9d9567d4afb2becd33c5ddf44935a6778
QueenMar <mfeudo@ucsc.edu>
2020-04-29 14:43:55 -0700

    Menu, Moving background, and platform physics from above

--- src/prefabs/Ground.js
+++ src/prefabs/Ground.js
@@ -1,6 +1,11 @@
-class Ground extends Phaser.GameObjects.Sprite{
+class Ground extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture, frame, pointValue) {
         super (scene, x, y, texture, frame);
+
+        scene.add.existing(this);               // add to existing scene, displayList, updateList
+        scene.physics.add.existing(this);       // add physics body
+                  
+        this.setImmovable();  
         scene.add.existing(this);
         this.newBarrier = true;  
     }
