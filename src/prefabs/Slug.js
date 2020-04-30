241722c0a958d84e5b5fccbe6678b82a81bf6941
Nicole Figg <nfigg@ucsc.edu>
2020-04-30 16:53:21 -0700

    Added Assets and Slug Prefab

---
+++ src/prefabs/Slug.js
@@ -0,0 +1,8 @@
+class Slug extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame){
+        super(scene,x,y,texture,frame);
+
+        scene.add.existing(this);
+    }
+
+}
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,8 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('virus', './assets/virus.png');
         this.load.image('ground', './assets/ground.png');
-        this.load.image('background', './assets/menuBackground.png');
+        this.load.image('menubackground', './assets/menuBackground.png');
 
     }
 
@@ -24,7 +23,7 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
 
         }
-        this.starfield = this.add.tileSprite(0, 0, 960, 640, 'background').setOrigin(0, 0);
+        this.starfield = this.add.tileSprite(0, 0, 960, 640, 'menubackground').setOrigin(0, 0);
         this.add.text(centerX, centerY, 'SLUG LIFE check', menuConfig).setOrigin(0.5);
         this.add.text(centerX, centerY - 64, 'Press Up Arrow to Jump', menuConfig).setOrigin(0.5);
         this.add.text(centerX, centerY - 128, 'Press L to Start', menuConfig).setOrigin(0.5);
