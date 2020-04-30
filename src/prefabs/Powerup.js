14cc2943bca4d955a6188e7b56ced41c842cf0a5
QueenMar <mfeudo@ucsc.edu>
2020-04-30 11:18:02 -0700

    Powerups written again bc deleted:(

--- src/prefabs/Powerup.js
+++ src/prefabs/Powerup.js
@@ -1,35 +1,27 @@
-class Ground extends Phaser.Physics.Arcade.Sprite{
+class Powerup extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture, frame, pointValue) {
         super (scene, x, y, texture, frame);
-
+        
         scene.add.existing(this);               // add to existing scene, displayList, updateList
         scene.physics.add.existing(this);       // add physics body
-                 
         this.setImmovable();  
         scene.add.existing(this);
-        this.newBarrier = true;  
+         this.setVelocity(-200); 
+         this.setGravity(0);
     }
 
 create(){
     this.atEnd = false;
-    this.setVelocity(200);
+    //this.body.setVelocity(200);
 }
 
 update(){
         // override physics sprite update()
+        //this.body.x = -4;
         super.update();
 
         // add new barrier when existing barrier hits center X
-        this.x -= game.settings.platformSpeed;;
-        if(this.newBarrier && this.x < centerX+300) {
-            this.newBarrier = false;
-            this.scene.addGround();
-        }
-
-        // destroy ground object if it reaches the left edge of the screen
-        if(this.x < -this.width-1000) { //-1000 so the starting ground platform doesn't despawn early
-            this.destroy();
-        }
+       
     }
 
     
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,13 +8,14 @@ Platforms do not have physics hooked up yet but they ARE all in a ground named p
 
 */
     preload(){
-
+        this.load.image('redbottle', './assets/redbottle.png');
         this.load.image('platform', './assets/platform.png');//placeholder image 
         this.load.image('platform1', './assets/platform1.png');//placeholder image 
         this.load.image('platform2', './assets/platform2.png');//placeholder image 
         this.load.image('platform3', './assets/platform3.png');//placeholder image 
         this.load.image('background1', './assets/background.png');//placeholder image 
         this.load.image('backgroundFront', './assets/backgroundFront.png');//placeholder image 
+        this.load.image('redAnimation', './assets/redAnimation.png');
     }
     create() {
         let playConfig = {
@@ -31,29 +32,35 @@ Platforms do not have physics hooked up yet but they ARE all in a ground named p
 
 
         }
-        
+        this.add.text(centerX, centerY - textSpacer * 4, 'SLUG LIFE PLAY SCENE 2', playConfig).setOrigin(0.5);
+
+
         this.ACCELERATION = 1500;
         this.MAX_X_VEL = 500;   // pixels/second
         this.MAX_Y_VEL = 5000;
         this.DRAG = 600;    // DRAG < ACCELERATION = icy slide
-        this.MAX_JUMPS = 1; // change for double/triple/etc. jumps 🤾‍♀️
+        this.MAX_JUMPS = 1; // change for double/triple/etc. jumps 🤾‍
         this.JUMP_VELOCITY = -700;
-        this.physics.world.gravity.y = 2600;
+        
+        
+       
 
         cursors = this.input.keyboard.createCursorKeys();
 
-        this.virus = this.physics.add.sprite(centerX, 0, 'virus');
-        this.virus.setGravityY(60);
-        this.virus.setCollideWorldBounds(true);
-       // this.virus.setBounce(0.1);
-        this.virus.setMaxVelocity(this.MAX_X_VEL, this.MAX_Y_VEL);
-        this.virus.isDestroyed = false;
+        this.background1 = this.add.tileSprite(0,0, 960, 640,'background1').setOrigin(0,0);
+        this.backgroundFront = this.add.tileSprite(0,0, 960, 640,'backgroundFront').setOrigin(0,0);
+
+        this.virus = new Virus(this, centerX, 0, 'virus');
+
+      // this.virus.body.gravity.y = 2600;
+        //this.virus.setMaxVelocity(this.MAX_X_VEL, this.MAX_Y_VEL);
+        //this.virus.setCollideWorldBounds(true);
+        
 
 
 
          
-        this.background1 = this.add.tileSprite(0,0, 960, 640,'background1').setOrigin(0,0);
-        this.backgroundFront = this.add.tileSprite(0,0, 960, 640,'backgroundFront').setOrigin(0,0);
+      
 
         //platforms spawn on the top half of the screen and the ground objects spawn on the bottom half
         this.platformGroup = this.add.group({
@@ -64,18 +71,9 @@ Platforms do not have physics hooked up yet but they ARE all in a ground named p
             runChildUpdate: true    // make sure update runs on group children
         });
 
-       // this.platformsPhysics = this.add.physicsGroup();
-        //this.groundPhysics = this.add.physicsGroup();
-
-
-        //this.platformsPhysics.setAll('body.allowGravity', false);
-       // this.platformsPhysics.setAll('body.immovable', true);
-        //this.groundPhysics.setAll('body.allowGravity', false);
-        //this.groundPhysics.setAll('body.immovable', true);
-        
-  
-
-
+        this.powerupGroup = this.add.group({
+            runChildUpdate: true
+        })
         
         this.platformClock = this.time.delayedCall(700, () => { //delay call to spawn second platform
             this.addPlatform();
@@ -88,21 +86,21 @@ Platforms do not have physics hooked up yet but they ARE all in a ground named p
 
         this.addGround();
 
-
-        this.add.text(centerX, centerY - textSpacer * 4, 'SLUG LIFE PLAY SCENE', playConfig).setOrigin(0.5);
+         
+        
 
         
 
 
-      //  this.ground = this.add.group();
-for (let i = 0; i < game.config.width; i += tileSize){
-      this.ground = this.physics.add.sprite(centerX, this.game.config.height * .95, 'ground');
-      //this.ground.displayWidth = this.game.config.width * 1.1;
-      //this.ground.setCollideWorldBounds(true);
-      this.ground.setCollideWorldBounds(true);
+//       //  this.ground = this.add.group();
+// for (let i = 0; i < game.config.width; i += tileSize){
+//       this.ground = this.physics.add.sprite(centerX, this.game.config.height * .95, 'ground');
+//       //this.ground.displayWidth = this.game.config.width * 1.1;
+//       //this.ground.setCollideWorldBounds(true);
+//       this.ground.setCollideWorldBounds(true);
 
-      this.ground.setImmovable();
-}
+//      // this.ground.setImmovable();
+// }
 
 
         //this.ground.setCollideWorldBounds(true);
@@ -112,14 +110,27 @@ for (let i = 0; i < game.config.width; i += tileSize){
 
 
 
-        this.ground.setImmovable();
+      //  this.ground.setImmovable();
         this.physics.add.collider(this.virus, this.ground1);
         this.physics.add.collider(this.virus, this.ground);
         this.physics.add.collider(this.virus, this.platformGroup);
         this.physics.add.collider(this.virus, this.groundGroup);
+        this.physics.add.collider(this.virus, this.powerupGroup);
         
+        
+       
         //this.groundGroup.setAll('body.immovable', true);
         //this.platformGroup.setAll('body.immovable', true);
+          this.powerupTest = new Powerup(this, 500, 500, 'redbottle',0);
+       
+          this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 5, first: 0}),
+            frameRate: 30
+        });
+       
+
+        //this.game.physics.arcade.collide(this.virus, this.powerupGroup, function(a,b){ b.destroyPowerup();}, null);
 
         }
     
@@ -133,6 +144,7 @@ addPlatform() {
     // create new platforms according to the height and skin parameters
     if(this.skinDesider == 1){
         this.platform =  new Platform(this, 1200, this.heightDesider, 'platform', 0).setScale(5, 0.5);
+        this.powerupAdd = new Powerup(this, 1200, this.heightDesider1-30, 'redbottle',0);
         
     } else if (this.skinDesider == 2){
 
@@ -153,6 +165,8 @@ addGround() {
     // create new barrier according to the height and skin parameters
     if(this.skinDesider1 == 1){
         this.ground =  new Ground(this, 1200, this.heightDesider1, 'platform', 0).setScale(5, 0.5);
+        this.powerupAdd = new Powerup(this, 1200, this.heightDesider1-30, 'redbottle',0);
+        this.powerupGroup.add(this.powerupAdd);
     } else if (this.skinDesider1 == 2){
 
         this.ground =  new Ground(this, 1200, this.heightDesider1, 'platform1', 0).setScale(5, 0.5); 
@@ -160,17 +174,23 @@ addGround() {
         this.ground =  new Ground(this, 1200, this.heightDesider1, 'platform2', 0).setScale(5, 0.5);
 
     }
-        
+    
     this.groundGroup.add(this.ground);  // add it to existing group
     //this.groundPhysics.add(this.platform);
 }
 
 
     update() {
-        this.background1.tilePositionX += 2;
-        this.backgroundFront.tilePositionX += 4;
-      this.ground1.x-= 6;
-
+        
+        if(this.powerup7 != null){
+            this.powerup7.x += 4;
+        }
+        
+    
+        this.background1.tilePositionX += .2;
+        this.backgroundFront.tilePositionX += .4;
+        this.ground1.x-= 6;
+        //this.powerupGroup.body.x -=4;
         if (!this.virus.isDestroyed) {
 
             if(this.virus.body.touching.down) {
@@ -187,15 +207,14 @@ addGround() {
                 this.jumps--;
                 this.jumping = false;
             }
-
-            
-            // if (cursors.up.isDown && this.virus.canJump && this.virus.body.touching.down) {
-
-            //     this.virus.setVelocityY(-100);
-            // }
         }
+       
+      
     }
 
+    destroyP(a,b){
+        console.log('destroy');
+    }
 
 
 
